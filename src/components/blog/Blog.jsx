028274d2d12a8bb7f18feb6bb73a2bc2a57fa6f5
USER <kirtrommarn@gmail.com>
2025-09-11 14:50:15 +0800

    Updated Blog Section and changed achievements images and texts

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -2,6 +2,7 @@ import img1 from "../../assets/images/blog/blog-1.jpg";
 import img2 from "../../assets/images/blog/blog-2.jpg";
 import img3 from "../../assets/images/blog/blog-3.jpg";
 import img4 from "../../assets/images/blog/blog-4.jpg";
+import img5 from "../../assets/images/blog/blog-5.jpg"; 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
 import MonoBlog from "./MonoBlog";
@@ -9,7 +10,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "./blog.css";
 
-// Breakpoints for swiperJS
+
 const custom_breakpoints = {
   640: {
     slidesPerView: 2,
@@ -29,61 +30,53 @@ const blogData = [
   {
     id: 1,
     image: img1,
-    date: "22 Oct, 2020",
+    date: "4 December, 2024",
     comments: 246,
-    title: "Designing Engaging User Interfaces for Modern Web Apps",
+    title: "Recognizing his active involvement and valuable insights during our User Acceptance Testing phase.",
     link: "/Achievements.html",
   },
   {
     id: 2,
     image: img2,
-    date: "22 Oct, 2020",
+    date: "9 December, 2024",
     comments: 246,
-    title: "Tips for Effective Dashboard Layouts and Usability",
+    title: "We sincerely thank Our Lady of Fatima University (OLFU) for inviting Mr. Tom Oliver Chua",
     link: "/Achievements.html",
   },
   {
     id: 3,
     image: img3,
-    date: "22 Oct, 2020",
+    date: "23 June, 2025",
     comments: 246,
-    title: "How to Visualize Data for Better Product Decisions",
+    title: "Being honored by Our Lady of Fatima University for their outstanding contributions",
     link: "/Achievements.html",
   },
   {
     id: 4,
     image: img4,
-    date: "22 Oct, 2020",
+    date: "09 July, 2025",
     comments: 246,
-    title: "Responsive Design: Adapting to All Devices Seamlessly",
+    title: "We extend our gratitude to the university for recognizing Supsoft Technologies and Mr. Tom Oliver Chua",
     link: "/Achievements.html",
   },
   {
     id: 5,
-    image: img2,
+    image: img5,
     date: "22 Oct, 2020",
     comments: 246,
-    title: "Streamlining Workflows with UI/UX Best Practices",
+    title: "We thank FEU Institute of Technology for the privilege of having Mr/ as a Plenary Speaker at 'Technofair and Pitch Presentation Day 2.",
     link: "/Achievements.html",
-  },
-  {
-    id: 6,
-    image: img1,
-    date: "22 Oct, 2020",
-    comments: 246,
-    title: "Optimizing Interface Components for Performance",
-    link: "/Achievements.html",
-  },
+  }
 ];
 
 const Blog = () => {
   return (
     <div className="content py-25 px-2 relative" id="blog">
       <div className="max-w-135 text-center mx-auto pb-17.5">
-        <p className="section-title pb-6">Blog</p>
+        <p className="section-title pb-6">Achievements</p>
         <p className="text-xs xs:text-[16px] md:text-lg text-gray-400">
-          Check out my recent blog posts where I share insights on design,
-          development, and the latest industry trends.
+          Explore my portfolio to see highlights of my achievements in design, 
+          development, and contributions to industry innovations.
         </p>
       </div>
       <Swiper
